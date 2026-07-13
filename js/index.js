6207b119a792e2033dba3404ccc9f7a538f40329
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-13 14:21:18 -0300

    feat: implement trailer modal functionality and replace iframe with HTML5 video element

--- js/index.js
+++ js/index.js
@@ -2,8 +2,16 @@ let botaoTrailer = document.querySelector(".botao-trailer");
 let botaoFecharModal = document.querySelector(".fechar-modal");
 let video = document.getElementById("video");
 let modal = document.querySelector(".modal");
-let linkDoVideo = "https://www.youtube.com/watch?v=Cb4WV4aXBpk";
+let linkDoVideo = "./vídeo/mario.mp4";
 
 function alternarModal(){ modal.classList.toggle("aberto"); }
-botaoTrailer.addEventListener("click", () => { alternarModal(); video.setAttribute("src",linkDoVideo); });
-botaoFecharModal.addEventListener("click", () => { alternarModal(); video.setAttribute("src",""); });
+botaoTrailer.addEventListener("click", () => { 
+  alternarModal(); 
+  video.setAttribute("src", linkDoVideo); 
+  video.play();
+});
+botaoFecharModal.addEventListener("click", () => { 
+  alternarModal(); 
+  video.pause();
+  video.setAttribute("src", ""); 
+});
