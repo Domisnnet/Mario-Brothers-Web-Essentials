7dc0110f8f4e077634fbfb67e042e3415e4ef628
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-13 14:05:57 -0300

    feat: implement trailer modal functionality and update font stylesheet link

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let botaoTrailer = document.querySelector(".botao-trailer");
 let botaoFecharModal = document.querySelector(".fechar-modal");
 let video = document.getElementById("video");
 let modal = document.querySelector(".modal");
-let linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+let linkDoVideo = "https://www.youtube.com/watch?v=Cb4WV4aXBpk";
 
 function alternarModal(){ modal.classList.toggle("aberto"); }
 botaoTrailer.addEventListener("click", () => { alternarModal(); video.setAttribute("src",linkDoVideo); });
