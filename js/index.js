e24be8c65bdd453f6f9513d73a6c1622f027c9fe
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-13 13:40:50 -0300

    feat: implement landing page for Super Mario Bros movie with interactive trailer modal

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let botaoTrailer = document.querySelector(".botao-trailer");
+let botaoFecharModal = document.querySelector(".fechar-modal");
+let video = document.getElementById("video");
+let modal = document.querySelector(".modal");
+let linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+
+function alternarModal(){ modal.classList.toggle("aberto"); }
+botaoTrailer.addEventListener("click", () => { alternarModal(); video.setAttribute("src",linkDoVideo); });
+botaoFecharModal.addEventListener("click", () => { alternarModal(); video.setAttribute("src",""); });
