92bce973382adbad43f2c1c8456e264bb4078c94
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-14 13:55:36 -0300

    feat: implement landing page with trailer modal and memory card game functionality

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ let botaoTrailer = document.querySelector(".botao-trailer");
 let botaoFecharModal = document.querySelector(".fechar-modal");
 let video = document.getElementById("video");
 let modal = document.querySelector(".modal");
-let linkDoVideo = "../vídeo/mario.mp4";
+const linkDoVideo = "/src/vídeo/mario.mp4";
 
 function alternarModal(){ modal.classList.toggle("aberto"); }
 botaoTrailer.addEventListener("click", () => { 
