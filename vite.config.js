4d005b34004e6c8aa63113aeb289e4fa38f45fa6
Domisnnet <dominique.goncalves61@gmail.com>
2026-07-15 11:11:17 -0300

    fix: update rollup input path for jogo.html to reflect directory structure change

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        jogo: path.resolve(__dirname, 'jogo.html'),
+        jogo: path.resolve(__dirname, 'src/page/jogo.html'),
       },
       output: {
         entryFileNames: (chunk) => {
